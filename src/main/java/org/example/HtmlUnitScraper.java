6ad44da4d5c30974c0f015bed76644c4d5749e38
dkarailiev <dkyuliyanov@gmail.com>
2022-11-19 15:36:25 +0200

    Removed test and gathered first page thread urls in a list

--- src/main/java/org/example/HtmlUnitScraper.java
+++ src/main/java/org/example/HtmlUnitScraper.java
@@ -2,47 +2,67 @@ package org.example;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
 public class HtmlUnitScraper {
-    public static void main(String[] args) throws IOException {
 
-        // Connect to a URL and return an HTML page
-        String url = "https://community.spotify.com/t5/Accounts/bd-p/spotifyaccountrelated";
-        HtmlPage htmlPage = connect(url);
+  public static void main(String[] args) throws IOException {
 
-        // Collect unique URLs
-        Set<String> anchorUrlSet = htmlPage.getAnchors().stream()
-                .map(htmlAnchor -> htmlAnchor.getAttribute("href"))
-                .collect(Collectors.toSet());
+    // Connect to a URL and return an HTML page (home page)
+    HtmlPage htmlPage = loadHtmlPage(UrlString.ACCOUNTS_BOARD_FIRST_PAGE);
+
+    // Collect URLs
+    Set<String> anchorStringUrlSet = collectUrls(htmlPage);
+
+    // Filter URLs by board type (Accounts)
+    List<String> filteredThreadUrlAnchorStringList = filterThreadUrlStrings(anchorStringUrlSet,
+        UrlString.ACCOUNT_PAGE_PREFIX);
+
+    // Create a list of thread URLs (first page) by appending the anchor bits to the home page suffix
+    List<String> threadUrlList = generateThreadUrls(UrlString.HOME_PAGE_SUFFIX,
+        filteredThreadUrlAnchorStringList);
+
+    threadUrlList.forEach(System.out::println);
+
+//    HtmlPage threadPage = loadHtmlPage(firstThreadUrl);
+
+    System.out.println();
+
+  }
+
+  private static List<String> generateThreadUrls(String homePageUrlSuffix,
+      List<String> filteredThreadUrlAnchorStringList) {
+    return filteredThreadUrlAnchorStringList.stream()
+        .map(str -> homePageUrlSuffix + str)
+        .toList();
+  }
 
 
-        // Filter URLs by board type
-        List<String> filteredThreadUrlList = anchorUrlSet.stream()
-                .filter(e -> e.startsWith("/t5/Accounts/"))
-                .toList();
+  private static List<String> filterThreadUrlStrings(Set<String> anchorStringUrlSet,
+      String boardPrefix) {
 
+    return anchorStringUrlSet.stream()
+        .filter(e -> e.startsWith(boardPrefix))
+        .toList();
+  }
 
+  private static Set<String> collectUrls(HtmlPage htmlPage) {
 
+    return htmlPage.getAnchors().stream()
+        .map(htmlAnchor -> htmlAnchor.getAttribute("href"))
+        .collect(Collectors.toSet());
+  }
 
-        System.out.println();
+  public static HtmlPage loadHtmlPage(String url) throws IOException {
+    WebClient webClient = new WebClient();
 
+    webClient.getOptions().setUseInsecureSSL(true);
+    webClient.getOptions().setCssEnabled(false);
+    webClient.getOptions().setJavaScriptEnabled(false);
 
-    }
-
-    public static HtmlPage connect(String url) throws IOException {
-        WebClient webClient = new WebClient();
-
-        webClient.getOptions().setUseInsecureSSL(true);
-        webClient.getOptions().setCssEnabled(false);
-        webClient.getOptions().setJavaScriptEnabled(false);
-
-        return webClient.getPage(url);
-    }
-
+    return webClient.getPage(url);
+  }
 }
